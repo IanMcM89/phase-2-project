0a4487fa27c57af426bdb550c9298b3ec0f0788d
Ian <ianmcm.89@gmail.com>
2021-12-29 03:07:52 -0500

    Changed header, main and footer div sizes and readjusted css percentages.

--- src/components/VegetableCard.js
+++ src/components/VegetableCard.js
@@ -3,7 +3,20 @@ import React from "react";
 function VegetableCard({ veggie }) {
     return (
         <div className="vegetable-card">
-          <p>{veggie.name}</p>
+          <div className="card-title">
+            <h3>{veggie.name}</h3>
+            <p>Posted By: {veggie.poster}</p>
+            <button>⭐</button>
+          </div>
+          <div className="card-content">
+            <img></img>
+            <div>
+              <p>Sunlight: </p>
+              <p>Water: </p>
+              <p>Season: </p>
+              <p>Description:</p>
+            </div>
+          </div>
         </div>
     );
 }
