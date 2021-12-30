c6de9a973ebb82b012ada9884a94ad754ec6a2dc
Ian <ianmcm.89@gmail.com>
2021-12-30 17:55:32 -0500

    Added github link to repo in footer component.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,10 @@ import React from "react";
 function Footer() {
   return (
     <footer id="app-footer">
-
+      <div id="github-link">
+        <a href="https://github.com/IanMcM89/phase-2-project">Created By IanMcM89</a>
+        <img src="./images/icon-github.png" alt="GitHub Logo"/>
+      </div>
     </footer>
   );
 }
